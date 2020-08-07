4db977146971013d286c2b6d8de40b12840a48c6
TJ Draper <tj@buzzingpixel.com>
2020-08-06 21:17:18 -0500

    Add hosts field to create new show page

---
+++ assets/js/Components/Selects.js
@@ -0,0 +1,26 @@
+/**
+ * @see https://joshuajohnson.co.uk/Choices/
+ * @see https://github.com/jshjohnson/Choices
+ */
+
+import Loader from '../Helpers/Loader.js';
+
+class Selects {
+    /**
+     * @param {NodeList} els
+     */
+    constructor (els) {
+        Loader.loadCss(
+            'https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css',
+        );
+
+        Loader.loadJs('https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/scripts/choices.min.js').then(() => {
+            els.forEach((el) => {
+                // eslint-disable-next-line no-undef,no-new
+                new Choices(el);
+            });
+        });
+    }
+}
+
+export default Selects;
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,19 @@ import SetGlobalData from './SetUp/SetGlobalData.js';
 import LoadAxios from './SetUp/LoadAxios.js';
 // import Analytics from './SetUp/Analytics.js';
 
+// Components
+import Selects from './Components/Selects.js';
+
 // Setup
 Events();
 SetGlobalData();
 LoadAxios();
 // Analytics();
+
+// Components
+
+// Selects
+const selectEls = document.querySelectorAll('[ref="select"]');
+if (selectEls.length > 0) {
+    new Selects(selectEls);
+}
