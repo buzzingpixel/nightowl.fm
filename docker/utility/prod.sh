a712c0b7cf2cb462217a8977fd4755a032413f01
TJ Draper <tj@buzzingpixel.com>
2020-09-12 23:07:36 -0500

    Add additional production configs

---
+++ docker/utility/prod.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p /log-volume/nginx;
+
+touch /log-volume/nginx/error.log;
+
+chmod -R 0777 /log-volume;
+
+while true; do
+    chmod -R 0777 /episodes-volume;
+    chmod -R 0777 /files-volume;
+    sleep 120;
+done
