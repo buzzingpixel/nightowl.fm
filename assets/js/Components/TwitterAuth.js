d424b85dc6c843c014512f4ee352a5c360b334c5
TJ Draper <tj@buzzingpixel.com>
2020-09-14 20:24:15 -0500

    Show error message when trying to auth with Twitter

--- assets/js/Components/TwitterAuth.js
+++ assets/js/Components/TwitterAuth.js
@@ -2,6 +2,14 @@ export default class {
     constructor (formEl) {
         this.formEl = formEl;
 
+        this.errorBanner = document.querySelector(
+            '[ref="errorBanner"]',
+        );
+
+        this.errorMessage = this.errorBanner.querySelector(
+            '[ref="errorMessage"]',
+        );
+
         formEl.addEventListener('submit', (e) => {
             this.submitAuth(e);
         });
@@ -18,8 +26,24 @@ export default class {
             .then((resp) => {
                 window.location = resp.data.url;
             })
-            .catch(() => {
-                window.location = '/cms/twitter/error';
+            .catch((error) => {
+                // window.location = '/cms/twitter/error';
+
+                let errorMsg = '';
+
+                JSON.parse(error.response.data).errors.forEach((i) => {
+                    if (i.code) {
+                        errorMsg += `<div class="font-bold mb-2">${i.code}</div>`;
+                    }
+
+                    if (i.message) {
+                        errorMsg += `<div class="mb-4">${i.message}</div>`;
+                    }
+                });
+
+                self.errorMessage.innerHTML = errorMsg;
+
+                self.errorBanner.classList.remove('hidden');
             });
     }
 }
