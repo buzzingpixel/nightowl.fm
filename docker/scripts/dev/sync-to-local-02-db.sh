9f82771d09691bc303b1af2f5e17393c7336fff1
TJ Draper <tj@buzzingpixel.com>
2024-03-05 21:16:25 -0600

    Remove old docker configuration

--- docker/scripts/dev/sync-to-local-02-db.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SQL_FILE_NAME="nightowl.psql";
-
-PGPASSWORD="${DB_PASSWORD}"
-
-pg_restore --clean -U "${DB_USER}" -d "${DB_DATABASE}" -v < "/opt/project/docker/localStorage/${SQL_FILE_NAME}"
